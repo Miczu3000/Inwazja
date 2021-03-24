6ac17ce8bc484295f69b67c6b6aa5bffed2526b6
Miczu3000 <d.krakus@gmail.com>
2021-03-24 20:33:23 +0100

    6

--- game_stats.py
+++ game_stats.py
@@ -3,7 +3,7 @@ class GameStats:
     def __init__(self, ai_game):
         self.settings = ai_game.settings
         self.reset_stats()
-        self.game_active = True
+        self.game_active = False
         self.dead_aliens = 0
         self.ship_dead = 0
     
